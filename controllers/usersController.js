32bed274bb2b4400496839663dc70defea68db8a
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-16 13:51:40 +0200

    Post form now is validated

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,17 @@
 const usersStorage = require("../storages/usersStorage");
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const validateUser = [
+	body("firstName").trim()
+		.isAlpha().withMessage(`First name ${alphaErr}`)
+		.isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+	body("lastName").trim()
+		.isAlpha().withMessage(`Last name ${alphaErr}`)
+		.isLength({min: 1, max: 10}).withMessage(`Last name ${lengthErr}`),
+]		
+
 
 function usersListGet(req, res) {
 	res.render("index", { title: "User list", users: usersStorage.getUsers(), });
@@ -8,10 +21,20 @@ function usersCreateGet(req, res) {
 	res.render("createUser", { title: "Create user", })
 }
 
-function usersCreatePost(req, res) {
-	const {firstName, lastName} = req.body;
-	usersStorage.addUser({firstName, lastName});
-	res.redirect("/");
-}
+const usersCreatePost = [
+	validateUser,
+	function usersCreatePost(req, res) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(404).render("createUser", {
+				title: "Create user",
+				errors: errors.array(),
+			});
+		}
+		const { firstName, lastName } = req.body;
+		usersStorage.addUser({ firstName, lastName });
+		res.redirect("/");
+	}
+]
 
-module.exports = {usersListGet, usersCreateGet, usersCreatePost}
+module.exports = { usersListGet, usersCreateGet, usersCreatePost }
