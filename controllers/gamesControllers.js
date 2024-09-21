89e580953e8c2a725b0ed8349c68206bfd2451f7
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-21 18:40:17 +0200

    Search for games seems to work

--- controllers/gamesControllers.js
+++ controllers/gamesControllers.js
@@ -3,7 +3,8 @@ const queries = require("../db/queries");
 //TO DO: Implement try/catch
 
 async function getGames(req, res) {
-	const query = await queries.getAllGames();
+	const reqParm = req.query.game;
+	const query = reqParm? await queries.getGame(reqParm) :await queries.getAllGames();
 	const results = query.map((value) => {
 		const date = new Date(value.release_date).toLocaleDateString("en-US", {
 			year: 'numeric',
--- db/queries.js
+++ db/queries.js
@@ -8,4 +8,11 @@ async function getAllGames() {
 	return (rows);
 }
 
-module.exports = { getAllGames };
+async function getGame(game) {
+	const { rows } = await pool.query("SELECT games.id, games.title, games.release_date, developers.name \
+										FROM games INNER JOIN developers ON games.dev_id=developers.id\
+											WHERE games.title ILIKE $1;", [`%${game}%`]);
+	return (rows);
+}
+
+module.exports = { getAllGames, getGame };
