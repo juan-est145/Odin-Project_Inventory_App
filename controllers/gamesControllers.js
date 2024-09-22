4c2119b41359f7ac62f32a14b2f3beaf7c23fd40
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-22 11:53:24 +0200

    Games get route is complete

--- controllers/gamesControllers.js
+++ controllers/gamesControllers.js
@@ -2,9 +2,7 @@ const queries = require("../db/queries");
 
 //TO DO: Implement try/catch
 
-async function getGames(req, res) {
-	const reqParm = req.query.game;
-	const query = reqParm? await queries.getGame(reqParm) :await queries.getAllGames();
+function listGames(query) {
 	const results = query.map((value) => {
 		const date = new Date(value.release_date).toLocaleDateString("en-US", {
 			year: 'numeric',
@@ -13,7 +11,36 @@ async function getGames(req, res) {
 		});
 		return (`Title: ${value.title} | Developer: ${value.name} | Release date: ${date}`);
 	});
+	return (results);
+}
+
+async function getGames(req, res) {
+	const reqParm = req.query.game;
+	const query = reqParm? await queries.getGame(reqParm) : null;
+	if (query === null) {
+		res.render("games", { array: [] });
+		return;
+	}
+	const results = listGames(query);
 	res.render("games", { array: results.length !== 0? results : null });
 }
 
-module.exports = { getGames };
+async function getAllGames(req, res) {
+	const query = await queries.getAllGames();
+	const results = listGames(query);
+	res.render("allGames", {array: results});
+}
+
+function listGames(query) {
+	const results = query.map((value) => {
+		const date = new Date(value.release_date).toLocaleDateString("en-US", {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		});
+		return (`Title: ${value.title} | Developer: ${value.name} | Release date: ${date}`);
+	});
+	return (results);
+}
+
+module.exports = { getGames, getAllGames };
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -3,5 +3,6 @@ const gamesRouter = Router();
 const gameControllers = require("../controllers/gamesControllers");
 
 gamesRouter.get("/", gameControllers.getGames);
+gamesRouter.get("/all", gameControllers.getAllGames);
 
 module.exports = gamesRouter;
