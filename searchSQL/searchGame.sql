b5b682194484bd4fa2b08d91edb172eba41d6dfb
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-10-05 16:54:57 +0200

    Deleted unnecessary files

--- searchSQL/searchGame.sql
+++
@@ -1,3 +0,0 @@
-SELECT games.id, games.title, games.release_date, developers.name 
-FROM games
-INNER JOIN developers ON games.dev_id=developers.id;
--- searchSQL/searchGameGenre.sql
+++
@@ -1,4 +0,0 @@
-SELECT games.title, genres.genre
-FROM game_genre
-	INNER JOIN games ON games.id=game_genre.game_id
-	INNER JOIN genres ON genres.id=game_genre.genre_id; 
